5a2806fd8ad25d5dac7889b7ef9fd17999526285
Kardast <dottorsf@gmail.com>
2022-07-21 18:30:20 +0200

    Improving and organizing code

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -21,8 +21,8 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function dashboard()
     {
-        return view('home');
+        return view('admin.dashboard');
     }
 }
